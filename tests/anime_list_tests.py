714eaed12b8321f5f0af22863e49b60eed3c1513
tomer gelber <tomergelber@gmail.com>
2015-05-30 21:38:29 +0300

    using unittest.TestCase's asserts

--- tests/anime_list_tests.py
+++ tests/anime_list_tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from unittest import TestCase
 
-from nose.tools import *
 import datetime
 
 import myanimelist.session
@@ -9,7 +9,7 @@ import myanimelist.media_list
 import myanimelist.anime_list
 
 
-class testAnimeListClass(object):
+class testAnimeListClass(TestCase):
     @classmethod
     def setUpClass(self):
         self.session = myanimelist.session.Session()
@@ -31,97 +31,152 @@ class testAnimeListClass(object):
 
         self.threger = self.session.anime_list(u'threger')
 
-    @raises(TypeError)
     def testNoUsernameInvalidAnimeList(self):
-        self.session.anime_list()
+        with self.assertRaises(TypeError):
+            self.session.anime_list()
 
-    @raises(myanimelist.media_list.InvalidMediaListError)
     def testNonexistentUsernameInvalidAnimeList(self):
-        self.session.anime_list(u'aspdoifpjsadoifjapodsijfp').load()
+        with self.assertRaises(myanimelist.media_list.InvalidMediaListError):
+            self.session.anime_list(u'aspdoifpjsadoifjapodsijfp').load()
 
     def testUserValid(self):
-        assert isinstance(self.shal, myanimelist.anime_list.AnimeList)
+        self.assertIsInstance(self.shal, myanimelist.anime_list.AnimeList)
 
     def testUsername(self):
-        assert self.shal.username == u'shaldengeki'
-        assert self.mona.username == u'monausicaa'
+        self.assertEqual(self.shal.username, u'shaldengeki')
+        self.assertEqual(self.mona.username, u'monausicaa')
 
     def testType(self):
-        assert self.shal.type == u'anime'
+        self.assertEqual(self.shal.type, u'anime')
 
     def testList(self):
-        assert isinstance(self.shal.list, dict) and len(self.shal) == 146
-        assert self.fz in self.shal and self.clannad in self.shal and self.trigun in self.shal
-        assert self.shal[self.fz][u'status'] == u'Watching' and self.shal[self.clannad][u'status'] == u'Completed' and \
-               self.shal[self.trigun][u'status'] == u'Plan to Watch'
-        assert self.shal[self.fz][u'score'] == None and self.shal[self.clannad][u'score'] == 9 and \
-               self.shal[self.trigun][u'score'] == None
-        assert self.shal[self.fz][u'episodes_watched'] == 6 and self.shal[self.clannad][u'episodes_watched'] == 23 and \
-               self.shal[self.trigun][u'episodes_watched'] == 6
-        assert self.shal[self.fz][u'started'] == None and self.shal[self.clannad][u'started'] == None and \
-               self.shal[self.trigun][u'started'] == None
-        assert self.shal[self.fz][u'finished'] == None and self.shal[self.clannad][u'finished'] == None and \
-               self.shal[self.trigun][u'finished'] == None
+        self.assertIsInstance(self.shal.list, dict)
+        self.assertEqual(len(self.shal), 146)
 
-        assert isinstance(self.pl.list, dict) and len(self.pl) >= 795
-        assert self.baccano in self.pl and self.pokemon in self.pl and self.dmc in self.pl
-        assert self.pl[self.baccano][u'status'] == u'Completed' and self.pl[self.pokemon][u'status'] == u'On-Hold' and \
-               self.pl[self.dmc][u'status'] == u'Dropped'
-        assert self.pl[self.baccano][u'score'] == 10 and self.pl[self.pokemon][u'score'] == None and self.pl[self.dmc][
-                                                                                                         u'score'] == 2
-        assert self.pl[self.baccano][u'episodes_watched'] == 13 and self.pl[self.pokemon][u'episodes_watched'] == 2 and \
-               self.pl[self.dmc][u'episodes_watched'] == 1
-        assert self.pl[self.baccano][u'started'] == datetime.date(year=2009, month=7, day=27) and self.pl[self.pokemon][
-                                                                                                      u'started'] == datetime.date(
-            year=2013, month=10, day=5) and self.pl[self.dmc][u'started'] == datetime.date(year=2010, month=9, day=27)
-        assert self.pl[self.baccano][u'finished'] == datetime.date(year=2009, month=7, day=28) and \
-               self.pl[self.pokemon][u'finished'] == None and self.pl[self.dmc][u'finished'] == None
+        self.assertIn(self.fz, self.shal)
+        self.assertIn(self.clannad, self.shal)
+        self.assertIn(self.trigun, self.shal)
 
-        assert isinstance(self.mona.list, dict) and len(self.mona) >= 1822
-        assert self.zombie in self.mona and self.lollipop in self.mona and self.musume in self.mona
-        assert self.mona[self.zombie][u'status'] == u'Completed' and self.mona[self.lollipop][
-                                                                         u'status'] == u'On-Hold' and \
-               self.mona[self.musume][u'status'] == u'Plan to Watch'
-        assert self.mona[self.zombie][u'score'] == 7 and self.mona[self.lollipop][u'score'] == None and \
-               self.mona[self.musume][u'score'] == None
-        assert self.mona[self.zombie][u'episodes_watched'] == 2 and self.mona[self.lollipop][
-                                                                        u'episodes_watched'] == 12 and \
-               self.mona[self.musume][u'episodes_watched'] == 0
-        assert self.mona[self.zombie][u'started'] == None and self.mona[self.lollipop][u'started'] == datetime.date(
-            year=2013, month=4, day=14) and self.mona[self.musume][u'started'] == None
-        assert self.mona[self.zombie][u'finished'] == None and self.mona[self.lollipop][u'finished'] == None and \
-               self.mona[self.musume][u'finished'] == None
+        self.assertEqual(self.shal[self.fz][u'status'], u'Watching')
+        self.assertEqual(self.shal[self.clannad][u'status'], u'Completed')
+        self.assertEqual(self.shal[self.trigun][u'status'], u'Plan to Watch')
 
-        assert isinstance(self.threger.list, dict) and len(self.threger) == 0
+        self.assertIsNone(self.shal[self.fz][u'score'])
+        self.assertEqual(self.shal[self.clannad][u'score'], 9)
+        self.assertIsNone(self.shal[self.trigun][u'score'])
+
+        self.assertEqual(self.shal[self.fz][u'episodes_watched'], 6)
+        self.assertEqual(self.shal[self.clannad][u'episodes_watched'], 23)
+        self.assertEqual(self.shal[self.trigun][u'episodes_watched'], 6)
+
+        self.assertIsNone(self.shal[self.fz][u'started'])
+        self.assertIsNone(self.shal[self.clannad][u'started'])
+        self.assertIsNone(self.shal[self.trigun][u'started'])
+
+        self.assertIsNone(self.shal[self.fz][u'finished'])
+        self.assertIsNone(self.shal[self.clannad][u'finished'])
+        self.assertIsNone(self.shal[self.trigun][u'finished'])
+
+        self.assertIsInstance(self.pl.list, dict)
+        self.assertGreaterEqual(len(self.pl), 795)
+
+        self.assertIn(self.baccano, self.pl)
+        self.assertIn(self.pokemon, self.pl)
+        self.assertIn(self.dmc, self.pl)
+
+        self.assertEqual(self.pl[self.baccano][u'status'], u'Completed')
+        self.assertEqual(self.pl[self.pokemon][u'status'], u'On-Hold')
+        self.assertEqual(self.pl[self.dmc][u'status'], u'Dropped')
+
+        self.assertEqual(self.pl[self.baccano][u'score'], 10)
+        self.assertIsNone(self.pl[self.pokemon][u'score'])
+        self.assertEqual(self.pl[self.dmc][u'score'], 2)
+
+        self.assertEqual(self.pl[self.baccano][u'episodes_watched'], 13)
+        self.assertEqual(self.pl[self.pokemon][u'episodes_watched'], 2)
+        self.assertEqual(self.pl[self.dmc][u'episodes_watched'], 1)
+
+        self.assertEqual(self.pl[self.baccano][u'started'], datetime.date(year=2009, month=7, day=27))
+        self.assertEqual(self.pl[self.pokemon][u'started'], datetime.date(year=2013, month=10, day=5))
+        self.assertEqual(self.pl[self.dmc][u'started'], datetime.date(year=2010, month=9, day=27))
+
+        self.assertEqual(self.pl[self.baccano][u'finished'], datetime.date(year=2009, month=7, day=28))
+        self.assertIsNone(self.pl[self.pokemon][u'finished'])
+        self.assertIsNone(self.pl[self.dmc][u'finished'])
+
+        self.assertIsInstance(self.mona.list, dict)
+        self.assertGreaterEqual(len(self.mona), 1822)
+
+        self.assertIn(self.zombie, self.mona)
+        self.assertIn(self.lollipop, self.mona)
+        self.assertIn(self.musume, self.mona)
+
+        self.assertEqual(self.mona[self.zombie][u'status'], u'Completed')
+        self.assertEqual(self.mona[self.lollipop][u'status'], u'On-Hold')
+        self.assertEqual(self.mona[self.musume][u'status'], u'Plan to Watch')
+
+        self.assertEqual(self.mona[self.zombie][u'score'], 7)
+        self.assertIsNone(self.mona[self.lollipop][u'score'])
+        self.assertIsNone(self.mona[self.musume][u'score'])
+
+        self.assertEqual(self.mona[self.zombie][u'episodes_watched'], 2)
+        self.assertEqual(self.mona[self.lollipop][u'episodes_watched'], 12)
+        self.assertEqual(self.mona[self.musume][u'episodes_watched'], 0)
+
+        self.assertIsNone(self.mona[self.zombie][u'started'])
+        self.assertEqual(self.mona[self.lollipop][u'started'], datetime.date(year=2013, month=4, day=14))
+        self.assertIsNone(self.mona[self.musume][u'started'])
+
+        self.assertIsNone(self.mona[self.zombie][u'finished'])
+        self.assertIsNone(self.mona[self.lollipop][u'finished'])
+        self.assertIsNone(self.mona[self.musume][u'finished'])
+
+        self.assertIsInstance(self.threger.list, dict)
+        self.assertEqual(len(self.threger), 0)
 
     def testStats(self):
-        assert isinstance(self.shal.stats, dict) and len(self.shal.stats) > 0
-        assert self.shal.stats[u'watching'] == 10 and self.shal.stats[u'completed'] == 102 and self.shal.stats[
-                                                                                                   u'on_hold'] == 1 and \
-               self.shal.stats[u'dropped'] == 5 and self.shal.stats[u'plan_to_watch'] == 28 and float(
-            self.shal.stats[u'days_spent']) == 38.88
+        self.assertIsInstance(self.shal.stats, dict)
+        self.assertGreater(len(self.shal.stats), 0)
+        self.assertEqual(self.shal.stats[u'watching'], 10)
+        self.assertEqual(self.shal.stats[u'completed'], 102)
+        self.assertEqual(self.shal.stats[u'on_hold'], 1)
+        self.assertEqual(self.shal.stats[u'dropped'], 5)
+        self.assertEqual(self.shal.stats[u'plan_to_watch'], 28)
+        self.assertEqual(float(self.shal.stats[u'days_spent']), 38.88)
 
-        assert isinstance(self.pl.stats, dict) and len(self.pl.stats) > 0
-        assert self.pl.stats[u'watching'] >= 0 and self.pl.stats[u'completed'] >= 355 and self.pl.stats[
-                                                                                              u'on_hold'] >= 0 and \
-               self.pl.stats[u'dropped'] >= 385 and self.pl.stats[u'plan_to_watch'] >= 0 and float(
-            self.pl.stats[u'days_spent']) >= 125.91
+        self.assertIsInstance(self.pl.stats, dict)
+        self.assertGreater(len(self.pl.stats), 0)
+        self.assertGreaterEqual(self.pl.stats[u'watching'], 0)
+        self.assertGreaterEqual(self.pl.stats[u'completed'], 355)
+        self.assertGreaterEqual(self.pl.stats[u'on_hold'], 0)
+        self.assertGreaterEqual(self.pl.stats[u'dropped'], 385)
+        self.assertGreaterEqual(self.pl.stats[u'plan_to_watch'], 0)
+        self.assertGreaterEqual(float(self.pl.stats[u'days_spent']), 125.91)
 
-        assert isinstance(self.mona.stats, dict) and len(self.mona.stats) > 0
-        assert self.mona.stats[u'watching'] >= 0 and self.mona.stats[u'completed'] >= 1721 and self.mona.stats[
-                                                                                                   u'on_hold'] >= 0 and \
-               self.mona.stats[u'dropped'] >= 0 and self.mona.stats[u'plan_to_watch'] >= 0 and float(
-            self.mona.stats[u'days_spent']) >= 470.30
+        self.assertIsInstance(self.mona.stats, dict)
+        self.assertGreater(len(self.mona.stats), 0)
+        self.assertGreaterEqual(self.mona.stats[u'watching'], 0)
+        self.assertGreaterEqual(self.mona.stats[u'completed'], 1721)
+        self.assertGreaterEqual(self.mona.stats[u'on_hold'], 0)
+        self.assertGreaterEqual(self.mona.stats[u'dropped'], 0)
+        self.assertGreaterEqual(self.mona.stats[u'plan_to_watch'], 0)
+        self.assertGreaterEqual(float(self.mona.stats[u'days_spent']), 470.30)
 
-        assert isinstance(self.threger.stats, dict) and len(self.threger.stats) > 0
-        assert self.threger.stats[u'watching'] == 0 and self.threger.stats[u'completed'] == 0 and self.threger.stats[
-                                                                                                      u'on_hold'] == 0 and \
-               self.threger.stats[u'dropped'] == 0 and self.threger.stats[u'plan_to_watch'] == 0 and float(
-            self.threger.stats[u'days_spent']) == 0.00
+        self.assertIsInstance(self.threger.stats, dict)
+        self.assertGreaterEqual(len(self.threger.stats), 0)
+        self.assertEqual(self.threger.stats[u'watching'], 0)
+        self.assertEqual(self.threger.stats[u'completed'], 0)
+        self.assertEqual(self.threger.stats[u'on_hold'], 0)
+        self.assertEqual(self.threger.stats[u'dropped'], 0)
+        self.assertEqual(self.threger.stats[u'plan_to_watch'], 0)
+        self.assertEqual(float(self.threger.stats[u'days_spent']), 0.00)
 
     def testSection(self):
-        assert isinstance(self.shal.section(u'Watching'), dict) and self.fz in self.shal.section(u'Watching')
-        assert isinstance(self.pl.section(u'Completed'), dict) and self.baccano in self.pl.section(u'Completed')
-        assert isinstance(self.mona.section(u'Plan to Watch'), dict) and self.musume in self.mona.section(
-            u'Plan to Watch')
-        assert isinstance(self.threger.section(u'Watching'), dict) and len(self.threger.section(u'Watching')) == 0
+        self.assertIsInstance(self.shal.section(u'Watching'), dict)
+        self.assertIn(self.fz, self.shal.section(u'Watching'))
+        self.assertIsInstance(self.pl.section(u'Completed'), dict)
+        self.assertIn(self.baccano, self.pl.section(u'Completed'))
+        self.assertIsInstance(self.mona.section(u'Plan to Watch'), dict)
+        self.assertIn(self.musume, self.mona.section(u'Plan to Watch'))
+        self.assertIsInstance(self.threger.section(u'Watching'), dict)
+        self.assertEqual(len(self.threger.section(u'Watching')), 0)
