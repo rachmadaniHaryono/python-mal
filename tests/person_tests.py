c6065e3b4c0e033dbab1b0a6862b855665fee98c
dani <foreturiga@gmail.com>
2015-09-30 09:08:40 +0800

    change test to raise error

--- tests/person_tests.py
+++ tests/person_tests.py
@@ -41,7 +41,8 @@ class testPersonClass(TestCase):
 
     def testNonExistentPerson(self):
         with self.assertRaises(myanimelist.person.InvalidPersonError):
-            self.session.person(4973204723047)
+            pe = self.session.person(4973204723047)
+            pe.load()
 
     def testPersonValid(self):
         self.assertIsInstance(self.hiroshi_kamiya, myanimelist.person.Person)
