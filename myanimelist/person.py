eca46bfa2e4d992580c7def9ca185084913aee82
dani <foreturiga@gmail.com>
2015-12-29 09:17:40 +0800

    flake 8 and fix too complex function when parsing

--- myanimelist/person.py
+++ myanimelist/person.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""Parser for person page."""
 import utilities
 from base import Base, MalformedPageError, InvalidBaseError, loadable
 
 
 class MalformedPersonPageError(MalformedPageError):
-    """Indicates that an person-related page on MAL has irreparably broken markup in some way.
-    """
+    """Indicates that an person-related page on MAL has irreparably broken markup in some way."""
+
     pass
 
 
 class InvalidPersonError(InvalidBaseError):
-    """Indicates that the requested person does not exist on MAL.
-    """
+    """Indicates that the requested person does not exist on MAL."""
+
     pass
 
 
 class Person(Base):
+    """Class for person."""
+
     def __init__(self, session, person_id):
+        """init person object given the session and person id."""
         super(Person, self).__init__(session)
         self.id = person_id
         if not isinstance(self.id, int) or int(self.id) < 1:
@@ -29,7 +33,7 @@ class Person(Base):
         self._published_manga = None
 
     def load(self):
-        """Fetches the MAL person page and sets the current person's attributes.
+        """Fetche the MAL person page and sets the current person's attributes.
 
         :rtype: :class:`.Person`
         :return: Current person object.
@@ -40,7 +44,7 @@ class Person(Base):
         return self
 
     def load_pictures(self):
-        """Fetches the MAL person pictures page and sets the current character's pictures attributes.
+        """Fetche the MAL person pictures page and sets the current character's pictures attributes.
 
         :rtype: :class:`.Person`
         :return: Current person object.
@@ -53,7 +57,7 @@ class Person(Base):
         return self
 
     def parse(self, person_page):
-        """Parses the DOM and returns person attributes in the main-content area.
+        """Parse the DOM and returns person attributes in the main-content area.
 
         :type character_page: :class:`bs4.BeautifulSoup`
         :param character_page: MAL person page's DOM
@@ -62,42 +66,54 @@ class Person(Base):
         :return: Person attributes.
 
         """
+        def merge_two_dicts(x, y):
+            """Given two dicts, merge them into a new dict as a shallow copy."""
+            z = x.copy()
+            z.update(y)
+            return z
         person_info = self.parse_sidebar(person_page)
+        person_info = merge_two_dicts(person_info, self.parse_name(person_page))
+        person_info = merge_two_dicts(person_info, self.parse_role(person_page))
+        person_info = merge_two_dicts(person_info, self.parse_position(person_page))
+        return person_info
 
-        def fix_name(name):
-            """Reverse the family name and given name in a name.
-            when a given name with comma inside it will reverse the order of the name.
-            ie 'abad, kajfk' to 'kajfk abad'
+    def _fix_name(self, name):
+                """Reverse the family name and given name in a name.
 
-            :type name: name
-            :param name: a person name
+                when a given name with comma inside it will reverse the order of the name.
+                ie 'abad, kajfk' to 'kajfk abad'
 
-            :rtype: dict
-            :return: fixed name.
-            """
-            if ',' not in name:
-                return name
-            else:
-                name_parts = name.split(',')
-                return '{} {}'.format(name_parts[1].strip(), name_parts[0].strip())
+                :type name: name
+                :param name: a person name
 
-        # parsing person name
+                :rtype: dict
+                :return: fixed name.
+                """
+                if ',' not in name:
+                    return name
+                else:
+                    name_parts = name.split(',')
+                    return '{} {}'.format(name_parts[1].strip(), name_parts[0].strip())
+
+    def parse_name(self, person_page):
+        """find name in person page."""
         try:
+            person_info = {}
             temp_name = person_page.select('div#contentWrapper h1.h1')[0].text
-            person_info[u'name'] = fix_name(temp_name)
+            person_info[u'name'] = self._fix_name(temp_name)
             if person_info[u'name'] == 'Invalid':
                 raise InvalidPersonError(self.id)
         except:
             if not self.session.suppress_parse_exceptions:
                 raise
+        return person_info
 
-        # parsing person role in anime
+    def parse_role(self, person_page):        # parsing person role in anime
+        """find role in person page."""
         try:
-            ''' # Fail with css selector
-            css_selector = 'div#contentWrapper div#content table tbody tr td table tr tbody tr'
-            va_roles_tags = person_page.select(css_selector)[0]
-            '''
-            table_sibling = [tag for tag in person_page.select('div.normal_header') if 'Voice Acting Roles' in tag.text][0]
+            person_info = {}
+            table_sibling = [tag for tag in person_page.select('div.normal_header')
+                             if 'Voice Acting Roles' in tag.text][0]
             va_roles_tags = table_sibling.parent.find('table').find_all('tr')
             va_roles = {}
             for va_tag in va_roles_tags:
@@ -115,7 +131,7 @@ class Person(Base):
                 char_tag = [xx for xx in a_tags if '/character/' in xx.get('href')][0]
                 # ie '/character/24502/Yuzuru_Otonashi'
                 character_id = int(char_tag.get('href').split('/')[2])
-                character_name = fix_name(char_tag.text)
+                character_name = self._fix_name(char_tag.text)
                 character_obj = self.session.character(character_id).set({'name': character_name})
 
                 # find the role for the said character ie 'main'
@@ -134,10 +150,15 @@ class Person(Base):
         except:
             if not self.session.suppress_parse_exceptions:
                 raise
+        return person_info
 
-        # parsing person position
+    def parse_position(self, person_page):
+        """find person position in anime production."""
         try:
-            table_sibling = [tag for tag in person_page.select('div.normal_header') if 'Anime Staff Positions' in tag.text][0]
+            person_info = {}
+            table_siblings = [tag for tag in person_page.select('div.normal_header')
+                              if 'Anime Staff Positions' in tag.text]
+            table_sibling = table_siblings[0]
             position_tables = table_sibling.parent.find_all('table')
             # check if position table only have  1table
             if len(position_tables) < 2:
@@ -181,7 +202,7 @@ class Person(Base):
         return person_info
 
     def parse_sidebar(self, person_page):
-        """Parses the DOM and returns person attributes in the sidebar.
+        """Parse the DOM and returns person attributes in the sidebar.
 
         :type character_page: :class:`bs4.BeautifulSoup`
         :param character_page: MAL person page's DOM
@@ -197,7 +218,7 @@ class Person(Base):
         return person_info
 
     def parse_pictures(self, picture_page):
-        """Parses the DOM and returns person pictures attributes.
+        """Parse the DOM and returns person pictures attributes.
 
         :type picture_page: :class:`bs4.BeautifulSoup`
         :param picture_page: MAL person pictures page's DOM
@@ -212,14 +233,17 @@ class Person(Base):
     @property
     @loadable(u'load')
     def name(self):
+        """name of the person."""
         return self._name
 
     @property
     @loadable(u'load')
     def voice_acting_roles(self):
+        """voice acting role done by this person."""
         return self._voice_acting_roles
 
     @property
     @loadable(u'load')
     def anime_staff_positions(self):
+        """Position of this person on anime production."""
         return self._anime_staff_positions
