1fc7d6c0f7bb2c8a2adaa96564a24c9072bbb183
dani <foreturiga@gmail.com>
2016-04-11 10:14:41 +0800

    chg: string checking when initiating tag obj.

--- myanimelist/tag.py
+++ myanimelist/tag.py
@@ -7,23 +7,39 @@ try:
 except ImportError:
     from .base import Base, MalformedPageError, InvalidBaseError
 
+
 class MalformedTagPageError(MalformedPageError):
+    """Error when parsing tag page."""
+
     pass
 
 
 class InvalidTagError(InvalidBaseError):
+    """error for invalid tag."""
+
     pass
 
 
 class Tag(Base):
+    """class for tag obj."""
+
     _id_attribute = "name"
 
     def __init__(self, session, name):
+        """init function."""
         super(Tag, self).__init__(session)
         self.name = name
-        if not isinstance(self.name, str) or len(self.name) < 1:
+        is_name_string = False
+        try:
+            if isinstance(self.name, unicode):
+                is_name_string = True
+        except NameError:
+            if isinstance(self.name, str):
+                is_name_string = True
+        if not is_name_string or len(self.name) < 1:
             raise InvalidTagError(self.name)
 
     def load(self):
+        """load all information from tag."""
         # TODO
         pass
