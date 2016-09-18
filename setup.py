9c7c33e47fc3bfed71e7a5f177c1d3e204cc3af3
rachmadaniHaryono <foreturiga@gmail.com>
2016-09-18 13:55:24 +0800

    add six libs.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ config = {
     'download_url': 'https://github.com/shaldengeki/python-mal/archive/master.zip',
     'author_email': package.__email__,
     'version': package.__version__,
-    'install_requires': ['beautifulsoup4', 'requests', 'pytz', 'lxml'],
+    'install_requires': ['beautifulsoup4', 'requests', 'pytz', 'lxml', 'six'],
     'tests_require': ['nose'],
     'packages': [NAME],
 }
