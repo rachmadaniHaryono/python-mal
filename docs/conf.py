dca89aba0df1b386e7268dfe2cf533950b23046e
rachmadaniHaryono <foreturiga@gmail.com>
2016-09-19 22:59:10 +0800

    Bump version: 0.1.7 → 0.1.8

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ copyright = u'2014, Charles Guo'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.7'
+version = '0.1.8'
 # The full version, including alpha/beta/rc tags.
 release = '0.1'
 
