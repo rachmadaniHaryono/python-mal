b61cbad0647b034402bfdd4e45ea44ce9ba6fc85
pushrbx <thomace44@gmail.com>
2017-07-26 17:23:35 +0100

    registered on pypi

--- setup.py
+++ setup.py
@@ -3,13 +3,22 @@ try:
 except ImportError:
   from distutils.core import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.markdown', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.markdown').read()
+
 config = {
-  'name': 'python3-mal_crawler',
+  'name': 'python3-mal',
   'description': 'Provides programmatic access to MyAnimeList resources.',
   'author': 'pushrbx',
+  'keywords': ['myanimelist', 'mal-api', 'mal python', 'myanimelist python', 'python3-mal', 'myanimelist api'],
+  'python_requires': '>=3.4, <4',
   'license': 'LICENSE.txt',
-  'url': 'https://github.com/shaldengeki/python-mal_crawler',
-  'download_url': 'https://github.com/pushrbx/python3-mal_crawler/archive/master.zip',
+  'long_description': long_description,
+  'url': 'https://github.com/pushrbx/python3-mal',
+  'download_url': 'https://github.com/pushrbx/python3-mal/archive/master.zip',
   'author_email': 'contact@pushrbx.net',
   'version': '0.2.7',
   'install_requires': ['urllib3==1.21.1', 'requests', 'pytz', 'lxml', 'cssselect'],
