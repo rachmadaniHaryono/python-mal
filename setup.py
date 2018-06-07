f5278184ac8057ffb27b4cb5c49c287ad7e9e8ce
pushrbx <contact@pushrbx.net>
2018-06-07 10:20:12 +0100

    Update setup.py
    dependencies version fixture

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ config = {
   'download_url': 'https://github.com/pushrbx/python3-mal/archive/master.zip',
   'author_email': 'contact@pushrbx.net',
   'version': '0.2.14',
-  'install_requires': ['urllib3', 'requests', 'pytz', 'lxml', 'cssselect'],
+  'install_requires': ['urllib3>=1.21.1,<1.23', 'requests<=2.18.4', 'pytz', 'lxml', 'cssselect'],
   'tests_require': ['nose'],
   'packages': ['myanimelist']
 }
